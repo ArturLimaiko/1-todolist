40b6be3052ac200a6e1b0854f6f0b2f962060270
Artur <alimayko91@gmail.com>
2024-08-14 11:39:34 +0300

    Спринт 01, неделя 2 . сделал доп задание ( сделал кнопку которая удаляет все таски) + переместил всю логику фильтрации тасок в компоненту Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,28 @@ export type TasksType = {
     isDone: boolean
 }
 
-export type FilterValuesType = 'all' | 'active' | 'completed'
+//Hi guys!
+//1. Let's create a 'DELETE ALL TASKS' button, and place it above the filter buttons
+//Clicking the button removes all tasks
+//2. Let's create a fourth filter button-if you click it, the first three tasks will be displayed
+//3. Relocate everything associated with  filters to the Todolist.tsx component. Make it work
+//
+// let [filter, setFilter] = useState<FilterValuesType>("all");
+//
+// let tasksForTodolist = tasks;
+//
+// if (filter === "active") {
+//     tasksForTodolist = tasks.filter(t => t.isDone === false);
+// }
+// if (filter === "completed") {
+//     tasksForTodolist = tasks.filter(t => t.isDone === true);
+// }
+//
+// function changeFilter(value: FilterValuesType) {
+//     setFilter(value);
+// }
+
+export type FilterValuesType = 'all' | 'active' | 'completed' | 'deleteAllTasks'
 
 function App() {
     // BLL
@@ -30,20 +51,6 @@ function App() {
         ]
     )
 
-    let tasksForTodoList = tasks
-    let [filter, setFilter] = useState<FilterValuesType>('all')
-    if (filter === 'active') {
-        tasksForTodoList = tasks.filter(t => !t.isDone)
-    }
-
-    if (filter === 'completed') {
-        tasksForTodoList = tasks.filter(t => t.isDone)
-    }
-
-    const changeFilter = (filter: FilterValuesType) => {
-        setFilter(filter)
-    }
-
     const removeTask = (taskId: number) => {
         let filteredTask = tasks.filter(task => task.id !== taskId)
         setTasks(filteredTask);
@@ -52,10 +59,9 @@ function App() {
     return (
         <div className="App">
             <div>
-                <Todolist tasks={tasksForTodoList}
+                <Todolist tasks={tasks}
                           title={'Whats to learn?'}
                           removeTask={removeTask}
-                          changeFilter={changeFilter}
                 />
             </div>
             {/*<div>*/}
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {AddTaskForm} from "./AddTaskForm";
 import {TasksList} from "./TasksList";
 import {FilterValuesType, TasksType} from "../App";
@@ -9,19 +9,38 @@ type TodoListsType = {
     tasks: TasksType[]
     title: string
     removeTask: (id: number) => void
-    changeFilter:(filter: FilterValuesType) => void
 }
 
-export const Todolist = ({tasks, title,removeTask,changeFilter}: TodoListsType) => {
+export const Todolist = ({tasks, title,removeTask}: TodoListsType) => {
+
+    let tasksForTodoList = tasks
+    let [filter, setFilter] = useState<FilterValuesType>('all')
+    if (filter === 'active') {
+        tasksForTodoList = tasks.filter(t => !t.isDone)
+    }
+
+    if (filter === 'completed') {
+        tasksForTodoList = tasks.filter(t => t.isDone)
+    }
+
+    if(filter === 'deleteAllTasks') {
+        tasksForTodoList = []
+    }
+
+    const changeFilter = (value: FilterValuesType) => {
+        setFilter(value)
+    }
+
     return (
         <>
             <TodolistHeader title={title}/>
             <AddTaskForm/>
-            <TasksList tasks={tasks} removeTask={removeTask}/>
+            <TasksList tasks={tasksForTodoList} removeTask={removeTask}/>
             <div>
                 <Button title='All' onClick={() => {changeFilter('all')}}/>
                 <Button title='Active' onClick={() => {changeFilter('active')}}/>
                 <Button title='Completed' onClick={() => {changeFilter('completed')}}/>
+                <Button title={'DeleteAllTasks'} onClick={() => changeFilter('deleteAllTasks')}/>
             </div>
         </>
     );
