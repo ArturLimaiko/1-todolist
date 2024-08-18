40fc4a3e44702539354abcf456774a44c4f8143f
Artur <alimayko91@gmail.com>
2024-08-19 00:15:45 +0300

    Спринт 01, неделя 4  добавил метод .trim() что бы при добавлении тасок убирались пробелы по бокам, например если случайно ввели пробел лишний

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -10,21 +10,24 @@ export const AddTaskForm = ({addTask}: AddTaskFormType) => {
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setTaskTitle(e.currentTarget.value)
 
-    const onClickHandler = () => {
-        addTask(taskTitle)
+    const addTaskHandler = () => {
+        // если пустая строка то не выполнится , если не пустая то выполнится
+        if (taskTitle.trim()) {
+            addTask(taskTitle.trim())
+        }
         setTaskTitle('')
     }
 
     const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-            onClickHandler()
+            addTaskHandler()
         }
     }
 
     return (
         <div>
             <input value={taskTitle} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
-            <Button title='+' onClick={onClickHandler}/>
+            <Button title='+' onClick={addTaskHandler}/>
         </div>
     );
 };
