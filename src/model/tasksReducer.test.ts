2e78cdab23fc994ddc5d4d5a6b740d590eeeb8de
Artur <alimayko91@gmail.com>
2024-09-27 20:59:48 +0300

    Спринт 03 - неделя 1  create addTaskAC,changeTaskStatusAC & tests

--- src/model/tasksReducer.test.ts
+++ src/model/tasksReducer.test.ts
@@ -1,5 +1,5 @@
 import {TasksStateType} from "../App";
-import {removeTaskAC, tasksReducer} from "./tasksReducer";
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "./tasksReducer";
 
 test('correct tasks should be deleted from correct array', () => {
     //стартовый стейт
@@ -34,4 +34,59 @@ test('correct tasks should be deleted from correct array', () => {
             ]
         }
     )
+})
+
+test('correct tasks should be added from correct array', () => {
+    //стартовый стейт
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'HTML&CSS', isDone: true},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'ReactJS', isDone: false},
+        ],
+        'todolistId2': [
+            {id: '1', title: 'NextJs', isDone: false},
+            {id: '2', title: 'StoryBook', isDone: true},
+            {id: '3', title: 'TS', isDone: true},
+        ]
+    }
+
+    //итоговый стейт
+    const action = addTaskAC('todolistId1', 'JQuery')
+    const endState = tasksReducer(startState, action)
+
+    // проверка  что наши действия в стейте соответствуют ожиданию -
+    // в нужном массиве добавится нужная таска
+    expect(endState['todolistId1'].length).toBe(4)
+    expect(endState['todolistId2'].length).toBe(3)
+    expect(endState['todolistId1'][3].id).toBeDefined()
+    expect(endState['todolistId1'][3].title).toBe('JQuery')
+    expect(endState['todolistId1'][3].isDone).toBe(false)
+})
+
+test('status of specified task should be  changed', () => {
+    //стартовый стейт
+    const startState: TasksStateType = {
+        'todolistId1': [
+            {id: '1', title: 'HTML&CSS', isDone: true},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'ReactJS', isDone: false},
+        ],
+        'todolistId2': [
+            {id: '1', title: 'NextJs', isDone: false},
+            {id: '2', title: 'StoryBook', isDone: true},
+            {id: '3', title: 'TS', isDone: true},
+        ]
+    }
+
+    //итоговый стейт
+    const action = changeTaskStatusAC('todolistId2', '1', true)
+    const endState = tasksReducer(startState, action)
+
+    // проверка  что наши действия в стейте соответствуют ожиданию -
+    // в нужном массиве добавится нужная таска
+    expect(endState['todolistId2'][0].isDone).toBeTruthy()
+    expect(endState['todolistId2'][1].isDone).toBe(true)
+    expect(endState['todolistId2'][1].isDone).toEqual(true)
+    expect(endState['todolistId2'][2].isDone).toEqual(true)
 })
--- src/model/tasksReducer.ts
+++ src/model/tasksReducer.ts
@@ -1,23 +1,48 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 export type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+export type AddTaskACType = ReturnType<typeof addTaskAC>
+export type changeTaskStatusAC = ReturnType<typeof changeTaskStatusAC>
 
 //все типы action
-export type ActionsType = RemoveTaskACType
+export type ActionsType = RemoveTaskACType | AddTaskACType | changeTaskStatusAC
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
-        case 'REMOVE-TASK': {
-            // setTasks({...tasks, [todolistId]: tasks[todolistId].filter(t => t.id !== taskId)}// если таска не равна => добавь ( иначе удалит)
+        case 'REMOVE-TASK':
+            //если таска не равна => добавь ( иначе удалит)
             return {
                 ...state,
                 [action.todolistId]: state[action.todolistId].filter(f => f.id !== action.taskId)
             }
+        case 'ADD-TASK': {
+            const newTask = {id: v1(), title: action.title, isDone: false}
+            return {
+                ...state,
+                [action.todolistId]: [...state[action.todolistId], newTask]
+            }
         }
-        default: throw new Error('Unknown action type')
+        case 'CHANGE-TASK-STATUS': {
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId]
+                    .map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
+            }
+        }
+        default:
+            throw new Error('Unknown action type')
     }
 }
 
 export const removeTaskAC = (taskId: string, todolistId: string) => {
     return {type: 'REMOVE-TASK', taskId, todolistId} as const
+}
+
+export const addTaskAC = (todolistId: string, title: string) => {
+    return {type: 'ADD-TASK', todolistId, title} as const
+}
+
+export const changeTaskStatusAC = (todolistId: string, taskId: string, isDone: boolean) => {
+    return {type: 'CHANGE-TASK-STATUS', todolistId, taskId, isDone} as const
 }
